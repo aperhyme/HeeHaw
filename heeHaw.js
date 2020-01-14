2b613c645f5861f9452bcaed8f59964cbab0f54d
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-14 00:40:53 -0500

    prints out results in the console

--- heeHaw.js
+++ heeHaw.js
@@ -5,11 +5,13 @@ for(let i=1; i <=100; i++){
     }
     else if((i%3 === 0)){
         console.log("Hee!")
+
     }
     else if(i%5 === 0){
         console.log("Haw!")
     }
     else{
-        console.log(i);
+        console.log(i)
+
     }
 }
